1f5da631a6c2caf4f6153f19f8378b8e8ecacc25
Roger Pence <roger.pence@gmail.com>
2020-08-09 13:24:39 -0500

    change tailwind config location to root

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const postcssimport = require('postcss-import');
 const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss')('./tailwind/tailwind.config.js');
+const tailwindcss = require('tailwindcss');
 
 module.exports  = {
     plugins: [
